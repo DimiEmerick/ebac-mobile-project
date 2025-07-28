529bf26ce242f457514b4bb5779aef234619947a
Dimi <114114664+DimiEmerick@users.noreply.github.com>
2025-07-28 15:58:16 -0300

    Collision Correction

--- Assets/Scripts/Items/ItemCollectableBase.cs
+++ Assets/Scripts/Items/ItemCollectableBase.cs
@@ -13,7 +13,7 @@ public class ItemCollectableBase : MonoBehaviour
     [Header("Sounds")]
     public AudioSource audioSource;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.transform.CompareTag(compareTag))
         {
